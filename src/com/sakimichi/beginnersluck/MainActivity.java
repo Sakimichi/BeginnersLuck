59d859dac7fdf4c423f3759f4c0cd6db01e0732c
Sakimichi <mae.banquil@gmail.com>
2014-11-21 08:02:45 +0800

    0.1
    Remove v4 and reference to appcompat_v7's v4 instead. Added second class
    and layout.

--- src/com/sakimichi/beginnersluck/MainActivity.java
+++ src/com/sakimichi/beginnersluck/MainActivity.java
@@ -1,9 +1,13 @@
 package com.sakimichi.beginnersluck;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this, Second.class));
+				
+			}
+		});
     }
 
 
@@ -19,6 +33,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        
         return true;
     }
 
